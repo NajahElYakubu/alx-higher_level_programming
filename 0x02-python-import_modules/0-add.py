05e3163185ef843d6b9861b97aeb458fe7cef8e4
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-22 06:04:52 -0700

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from add_0 import add
+if __name__ == '__main__':
+    add(
+            a = 1,
+            b = 2
+        )
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {(a + b)}")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+tasks on import and modules in python
