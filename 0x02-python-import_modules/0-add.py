69dd77a9b395c034f92d1844892ed163db22beff
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-22 06:15:18 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,6 @@
 
 from add_0 import add
 if __name__ == '__main__':
-    add(
-            a = 1,
-            b = 2
-        )
     a = 1
     b = 2
-    print(f"{a} + {b} = {(a + b)}")
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
