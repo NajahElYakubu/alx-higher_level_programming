41536873cb5bb97062b13dae57898cd1047a1b3a
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-22 13:47:20 -0700

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def print_name():
+    name = dir(hidden_4)
+    for i in name:
+        if i != '__':
+            print("{:S}".format(i))
+
+
+if __name__ == "__main__":
+    print_name()
