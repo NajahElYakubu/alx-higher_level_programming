8a0523b36ccf9930bf9c1239096a0e6b3a96d07e
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-25 08:47:33 -0700

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     i = len(my_list) - 1
+    if idx > i or idx < 0:
+        return(my_list)
     my_list[idx] = element
-    if idx < 0 or idx > i:
-        return(my_list)
-    else:
-        return(my_list)
+    return(my_list)
 
 
 if __name__ == "__main__":
