554c75b06af003b618e694c48481daf60c5bd5bf
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-25 07:44:03 -0700

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,14 +2,13 @@
 
 
 def element_at(my_list, idx):
-    i = len(my_list)
-    element = my_list[idx]
+    i = len(my_list) - 1
     if idx < 0:
         return None
     elif idx > i:
         return None
     else:
-        return (element)
+        return (my_list[idx])
 
 
 if __name__ == "__main__":
