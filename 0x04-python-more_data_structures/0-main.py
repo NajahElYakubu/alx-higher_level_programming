609e2505305ddf8da8e349580032438ddddaf2fb
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-28 15:05:18 +0000

    task 0

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-quare_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
-=======
 #!/usr/bin/python3
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
->>>>>>> 71d21cb14f158b3c4642e85000e8fc155d221031
 
 matrix = [
     [1, 2, 3],
@@ -14,7 +10,3 @@ matrix = [
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
 print(matrix)
-<<<<<<< HEAD
-
-=======
->>>>>>> 71d21cb14f158b3c4642e85000e8fc155d221031
