38becacaca68718b3dd1911e9f3f648933aafcd5
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-28 10:15:41 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	tmp = []
-	i = len(tmp)
-	for x in my_list:
-		tmp.append(x)
-	i += 1
-	n_list = tmp
-	z = 0
-	for y in n_list:
-		if n_list[z] == search:
-			n_list[z] = replace
-		z += 1
-	return n_list
+    tmp = []
+    i = len(tmp)
+    for x in my_list:
+        tmp.append(x)
+        i += 1
+    n_list = tmp
+    z = 0
+    for y in n_list:
+        if n_list[z] == search:
+            n_list[z] = replace
+        z += 1
+    return n_list
