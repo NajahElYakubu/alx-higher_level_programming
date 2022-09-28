d5dfb0de611bbee90b3d3510e6b1f17932a7fd65
Najah El-yakubu <najahelyakubu21@gmail.com>
2022-09-28 15:50:29 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/bin/usr/python3
 def square_matrix_simple(matrix=[]):
-    n_matrix = []
-    for i in matrix:
-        n_matrix.append(list(map(lambda i: i**2, i)))
-    return (n_matrix)
+	n_matrix = matrix
+	for i in range(len(matrix)):
+		for j in range(len(matrix[i])):
+			n_matrix[i][j] = matrix[i][j] ** 2
+	return n_matrix  
