1b6dda1e05fc4b33ee90a72f7b0985d098335df7
NajahElYakubu <najahelyakubu21@gmail.com>
2022-12-01 11:17:44 -0800

    tasks

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,11 @@
 -- Uses a database to list a "value"
 SELECT g.name
 FROM tv_genres AS g
-	INNER JOIN tv_show_genres AS t
-	ON g.id = t.genre_id
+	INNER JOIN tv_show_genres AS s
+	ON g.id = s.genre_id
 
 
-	INNER JOIN tv_shows as s
-	ON s.id = t.show_id
+	INNER JOIN tv_shows as t
+	ON t.id = s.show_id
 	WHERE s.title = 'Dexter'
 ORDER BY g.name;
